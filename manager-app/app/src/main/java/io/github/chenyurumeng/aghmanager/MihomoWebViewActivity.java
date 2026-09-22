d3a47c90356a9e94504b180f7430b55a4e60fa6b
chenyurumeng <79356303+chenyurumeng@users.noreply.github.com>
2026-09-22 16:32:30 +0800

    fix(manager): isolate Mihomo WebView session

---
+++ manager-app/app/src/main/java/io/github/chenyurumeng/aghmanager/MihomoWebViewActivity.java
@@ -0,0 +1,8 @@
+package io.github.chenyurumeng.aghmanager;
+
+public class MihomoWebViewActivity extends WebViewActivity {
+    @Override
+    protected String dataDirectorySuffix() {
+        return "mihomo_web";
+    }
+}
