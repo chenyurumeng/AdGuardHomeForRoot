de5a04d1794d0bbd320a63b0a979cc539c377253
chenyurumeng <79356303+chenyurumeng@users.noreply.github.com>
2026-09-22 16:32:28 +0800

    fix(manager): isolate Foreign AGH WebView session

---
+++ manager-app/app/src/main/java/io/github/chenyurumeng/aghmanager/ForeignWebViewActivity.java
@@ -0,0 +1,8 @@
+package io.github.chenyurumeng.aghmanager;
+
+public class ForeignWebViewActivity extends WebViewActivity {
+    @Override
+    protected String dataDirectorySuffix() {
+        return "agh_foreign";
+    }
+}
